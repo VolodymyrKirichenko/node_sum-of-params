cf14925d57a1492e8cdec41902984dcee0f3013c
Sergio k <valodya.kirik@gmail.com>
2023-01-10 16:01:29 +0200

    project has been added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const operation = process.argv[2];
+const args = process.argv.slice(3).filter(el => Number(el));
+
+function calculation(action, array) {
+  if (!array.length) {
+    return `The is nothing to ${action}`;
+  }
+
+  switch (action) {
+    case 'add':
+      return array.reduce((acc, el) => Number(acc) + Number(el), 0);
+
+    case 'multiply':
+      return array.reduce((acc, el) => Number(acc) * Number(el), 1);
+
+    default:
+      return 'Operation is not supported';
+  }
+}
+
+calculation(operation, args);
+console.log(calculation(operation, args));
